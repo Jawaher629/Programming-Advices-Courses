3e4a27beabda25a1e536d8b9cc148c64499ea257
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2025-08-29 19:03:31 +0300

    Create Problem #12: check Typical Matrices.cpp

---
+++ 07 - Algorithms & Problem Solving Level 3/Problem #12: check Typical Matrices.cpp
@@ -0,0 +1,94 @@
+#include <ctime>
+#include <iostream>
+#include <iomanip>
+#include <array>
+using namespace std;
+
+
+
+int RandomeNumber(int from, int to)
+{
+	int randomeNumber = rand() % (to - from + 1) + from;
+	return randomeNumber;
+}
+
+
+void FillMatrixWithRandomeNums(int arr[3][3], short Rows, short Columns)
+{
+	for (int i = 0; i < Rows; i++)
+	{
+		for (int j = 0; j < Columns; j++)
+		{
+			arr[i][j] = RandomeNumber(1, 10);
+		}
+	}
+}
+
+
+bool AretypicalMatrices(int arr[3][3],int arr2[3][3], short Rows, short Columns)
+{
+	for (short i = 0; i < Rows; i++)
+	{
+		for (int j = 0; j < Columns; j++)
+
+		{
+			if (arr[i][j] != arr2[i][j])
+			{
+				return  false;
+			}
+		}
+	}
+	return true;
+}
+
+
+
+void PrintMatrix(int arr[3][3], short Rows, short Columns)
+{
+
+	for (int C = 0; C < Rows; C++)
+	{
+		for (int R = 0; R < Rows; R++)
+		{
+			printf("%0*d      ", 2, arr[C][R]);
+		}
+		cout << endl << endl;
+	}
+}
+
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	int arr[3][3] , arr2[3][3];
+
+
+	FillMatrixWithRandomeNums(arr, 3, 3);
+	cout << "Matrix 1 Elements: \n" << "------------------------------\n";
+	PrintMatrix(arr, 3, 3);
+	cout << "------------------------------\n";
+
+	FillMatrixWithRandomeNums(arr2, 3, 3);
+	cout << "Matrix 2 Elements: \n" << "------------------------------\n";
+	PrintMatrix(arr2, 3, 3);
+	cout << "------------------------------\n";
+
+	if (AretypicalMatrices(arr, arr2, 3, 3))
+	{
+		cout << "Yes, they are typical.\n";
+	}
+	else
+		cout << "No,they are not typical.\n";
+
+
+
+	
+
+	system("pause>0");
+
+
+
+
+
+	return 0;
+}
