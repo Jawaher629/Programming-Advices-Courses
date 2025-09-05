b9bd0f8615156f794e41fe988859e8abb262ee4e
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2025-09-05 16:49:25 +0300

    Create Problem #19: Find Min \ Max Number in matrix.cpp

---
+++ 07 - Algorithms & Problem Solving Level 3/Problem #19: Find Min \ Max Number in matrix.cpp
@@ -0,0 +1,104 @@
+#include <ctime>
+#include <iostream>
+#include <iomanip>
+#include <array>
+using namespace std;
+
+
+int RandomeNumber(int from, int to)
+{
+	int randomeNumber = rand() % (to - from + 1) + from;
+	return randomeNumber;
+}
+
+
+void FillMatrixWithRandomeNums(int arr[3][3], short Rows, short Columns)
+{
+	for (int i = 0; i < Rows; i++)
+	{
+		for (int j = 0; j < Columns; j++)
+		{
+			arr[i][j] = RandomeNumber(1, 10);
+		}
+	}
+}
+
+
+
+
+void PrintMatrix(int arr[3][3], short Rows, short Columns)
+{
+
+	for (int C = 0; C < Rows; C++)
+	{
+		for (int R = 0; R < Rows; R++)
+		{
+			printf("%0*d      ", 2, arr[C][R]);
+		}
+		cout << endl << endl;
+	}
+}
+
+
+bool IsNumExistsInMatrix(int arr[3][3], int Num, short Rows, short Columns)
+{
+	for (int i = 0; i < Rows; i++)
+	{
+		for (int j = 0; j < Columns; j++)
+		{
+			if (arr[i][j] == Num)
+			{
+
+				return true;
+			};
+		}
+	}
+	return false;
+}
+
+
+
+
+
+void PrintMinAndMaxNum (int arr[3][3], short Rows, short Columns)
+{
+	int MaxNumber = arr[0][0];
+	int MinNumber = arr[0][0];
+
+	for (int i = 0; i < Rows; i++)
+	{
+		for (int j = 0; j < Columns; j++)
+		{
+			if (MaxNumber <= arr[i][j])
+				MaxNumber = arr[i][j];
+
+			if (MinNumber >= arr[i][j])
+				MinNumber = arr[i][j];
+		}
+	}
+
+	cout << "Maximum Number is: " << MaxNumber << endl;
+	cout << "Minimum Number is: " << MinNumber << endl;
+}
+
+
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	int arr[3][3];
+
+	FillMatrixWithRandomeNums(arr, 3, 3);
+
+	cout << "Matrix 1 Elements: \n" << "------------------------------\n";
+	PrintMatrix(arr, 3, 3);
+	cout << "------------------------------\n";
+
+
+	PrintMinAndMaxNum(arr,  3, 3);
+	
+
+
+	system("pause>0");
+	return 0;
+}
