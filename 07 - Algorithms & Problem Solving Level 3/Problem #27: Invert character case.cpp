b0992fb1bbe56013e97bfd2c1431350aa763c2e4
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2025-10-25 20:39:34 +0300

    Update Problem #27: Invert character case.cpp

--- 07 - Algorithms & Problem Solving Level 3/Problem #27: Invert character case.cpp
+++ 07 - Algorithms & Problem Solving Level 3/Problem #27: Invert character case.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+char ReadUsersChar(string Message)
+{
+	char Sent;
+	cout << Message;
+	cin >> Sent;
+	return Sent;
+}
+
+
+char InvertChar(char Char1)
+{
+	return isupper(Char1) ? tolower(Char1): toupper(Char1);
+}
+
+
+
+
+int main()
+{
+	char Char1 = ReadUsersChar("Enter your character to invert: ");
+
+	cout << "Character inverted to upper / lower case: \n";
+	Char1 = InvertChar(Char1);
+  
+	cout << Char1 << endl << endl;
+
+
+	system("pause>0");
+	return 0;
+}
