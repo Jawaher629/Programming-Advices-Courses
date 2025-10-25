5655b0b79a24ce72b0df5680b98e97c01bc4eb1d
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2025-10-25 21:43:14 +0300

    Update Problem #28: Invert all letters  case.cpp

--- 07 - Algorithms & Problem Solving Level 3/Problem #28: Invert all letters  case.cpp
+++ 07 - Algorithms & Problem Solving Level 3/Problem #28: Invert all letters  case.cpp
@@ -1,1 +1,52 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+
+char InvertChar(char Char1)
+{
+	return isupper(Char1) ? tolower(Char1) : toupper(Char1);
+}
+
+
+string ReadUsersString(string Message)
+{
+	string Sent;
+	cout << Message;
+	getline(cin,Sent);
+	return Sent;
+}
+
+
+string InvertAlltLetterOfString(string Sent)
+{
+
+
+	for (short i = 0; i < Sent.length(); i++)
+	{
+		if (Sent[i] != ' ')
+		{
+			Sent[i] = InvertChar(Sent[i]);
+		}
+		
+	}
+	return Sent;
+}
+
+
+
+
+
+
+int main()
+{
+	string Sent1 = ReadUsersString("Enter your sentense to invert: ");
+
+	cout << "String after Inverting All Letters Case: \n";
+	Sent1 = InvertAlltLetterOfString(Sent1);
+	cout << Sent1 << endl << endl;
+
+
+	system("pause>0");
+	return 0;
+}
