d14fd53c6158e02e60fc9e95cd406bd5d20e7668
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2025-10-25 22:27:41 +0300

    Create Problem #29: Count small \ capital letters.cpp

---
+++ 07 - Algorithms & Problem Solving Level 3/Problem #29: Count small \ capital letters.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+char InvertChar(char Char1)
+{
+	return isupper(Char1) ? tolower(Char1) : toupper(Char1);
+}
+
+
+string ReadUsersString(string Message)
+{
+	string Sent;
+	cout << Message;
+	getline(cin,Sent);
+	return Sent;
+}
+
+
+int CountStringLetter(string Sentence1)
+{
+	return Sentence1.length();
+}
+
+short CountCapitalLetter(string Sent)
+{
+	short Counter = 0;
+	for (short i = 0; i < Sent.length(); i++)
+	{
+		if (isupper(Sent[i]))
+		{
+			Counter++;
+		}
+	}
+	return Counter;
+}
+
+
+short CountSmalllLetter(string Sent)
+{
+	short Counter = 0;
+	for (short i = 0; i < Sent.length(); i++)
+	{
+		if (islower(Sent[i]))
+		{
+			Counter++;
+		}
+	}
+	return Counter;
+}
+
+
+
+int main()
+{
+	string Sent1 = ReadUsersString("Enter your sentense to invert: ");
+	cout << "Sentence's letters count: " << Sent1.length() << endl;
+	cout << "Capital letters count: " << CountCapitalLetter(Sent1) << endl;
+	cout << "Small letters count: " << CountSmalllLetter(Sent1) << endl;
+
+
+
+
+	system("pause>0");
+	return 0;
+}
