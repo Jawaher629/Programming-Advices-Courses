d3bec43d4e1360f3d192411b67d44435c39dc683
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2025-11-07 00:58:43 +0300

    Update Problem #34: print all vowels in a sentence.cpp

--- 07 - Algorithms & Problem Solving Level 3/Problem #34: print all vowels in a sentence.cpp
+++ 07 - Algorithms & Problem Solving Level 3/Problem #34: print all vowels in a sentence.cpp
@@ -1,3 +1,4 @@
+// My Solution: 
 #include <iostream>
 #include <string>
 #include "My_Input_Lib.h"
@@ -31,6 +32,49 @@ int main()
 {
 
 	cout << "- Vowels in the sentence: " << PrintVowelsInAstring(My_Input_Lib::ReadUsersString("Enter your sentence: "));
+	
+
+	system("pause>0");
+	return 0;
+}
+
+
+// Dr.Abo Hadhoud Solution:
+
+#include <iostream>
+#include <string>
+#include "My_Input_Lib.h"
+using namespace std;
+
+
+
+
+bool IsVowelLetter(char Letter)
+{
+	Letter = tolower(Letter);
+	return Letter == 'a' || Letter == 'i' || Letter == 'o' || Letter == 'u' || Letter == 'e';
+}
+
+
+void  PrintVowelsInAstring(string Sent1)
+{
+	string Vowels = "";
+	for (short i = 0; i < Sent1.length(); i++)
+
+	{
+		if (IsVowelLetter(Sent1.at(i)))
+			cout << Sent1[i] << "  ";
+	}
+}
+
+
+
+
+int main()
+{
+	string Sent1 = My_Input_Lib::ReadUsersString("Enter your sentence: ");
+	cout << "- Vowels in the sentence: " ;
+	PrintVowelsInAstring(Sent1);
 	
 
 	system("pause>0");
