d98cef038ae1bd9ace167c00481672121cc0d73e
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2025-11-19 01:14:22 +0300

    Update Problem #37: Split function.cpp

--- 07 - Algorithms & Problem Solving Level 3/Problem #37: Split function.cpp
+++ 07 - Algorithms & Problem Solving Level 3/Problem #37: Split function.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "My_Input_Lib.h"
+using namespace std;
 
+
+vector <string> SplitString(string Sent1 ,string delim )
+{
+	vector <string> vWords;
+	string Word; short pos = 0 ;
+
+	while ((pos = Sent1.find(delim)) != std::string::npos)
+	{
+		Word = Sent1.substr(0, pos);
+		if (Word != "")
+		{
+				vWords.push_back(Word) ;
+		}
+		Sent1.erase(0, pos + delim.length());
+	}
+
+
+	if (Sent1 != " ")
+		vWords.push_back(Sent1);
+
+	return vWords;
+}
+
+
+int main()
+{
+	vector <string> vWords;
+	string Sent1 = My_Input_Lib::ReadUsersString("Enter your sentence: ");
+	vWords = SplitString(Sent1, " ");
+	cout << "- Token: " << vWords.size() << "\n";
+	cout << "- Words: \n";
+	for (string &word : vWords)
+	{
+		cout << word << endl;
+	}
+	system("pause>0");
+	return 0;
+}
