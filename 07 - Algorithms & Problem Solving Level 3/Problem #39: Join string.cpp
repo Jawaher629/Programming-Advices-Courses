09976978f7a19c296d0a1a737ea9a42640456683
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2025-11-24 01:41:55 +0300

    Update Problem #39: Join string.cpp

--- 07 - Algorithms & Problem Solving Level 3/Problem #39: Join string.cpp
+++ 07 - Algorithms & Problem Solving Level 3/Problem #39: Join string.cpp
@@ -1,1 +1,25 @@
+// My Solution:
+#include <iostream>
+#include <string>
+#include <vector>
 
+string JoinString(vector <string> vWords, string delim)
+{
+	
+	string sent = "";
+	for (short i = 0 ; i < vWords.size();i++)
+	      sent = sent + vWords[i] + delim;
+
+
+	return (sent.substr(0,sent.length()-delim.length()));
+}
+
+
+int main()
+{
+	vector <string> vWords = {"Jawaher","math","Physics"};
+	cout << JoinString(vWords, "\n");
+
+	system("pause>0");
+	return 0;
+}
