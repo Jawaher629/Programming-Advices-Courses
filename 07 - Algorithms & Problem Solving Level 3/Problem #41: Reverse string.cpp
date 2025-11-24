b9fcb69d7a88e212e715afc98fa16069e660b367
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2025-11-25 01:47:57 +0300

    Update Problem #41: Reverse string.cpp

--- 07 - Algorithms & Problem Solving Level 3/Problem #41: Reverse string.cpp
+++ 07 - Algorithms & Problem Solving Level 3/Problem #41: Reverse string.cpp
@@ -44,3 +44,59 @@ int main()
 }
 
 //Dr.Abu Hadhoud solution:
+#include <iostream>
+#include <string>
+#include <vector>
+#include "My_Input_Lib.h"
+using namespace std;
+
+vector <string> SplitString(string Sent1, string delim)
+{
+	vector <string> vWords;
+	string Word; short pos = 0;
+
+	while ((pos = Sent1.find(delim)) != std::string::npos)
+	{
+		Word = Sent1.substr(0, pos);
+		if (Word != "")
+		{
+			vWords.push_back(Word);
+		}
+		Sent1.erase(0, pos + delim.length());
+	}
+
+
+	if (Sent1 != " ")
+		vWords.push_back(Sent1);
+
+	return vWords;
+}
+
+string ReverseWords(string sent1)
+{
+	vector <string> vString = SplitString(sent1, " ");
+	string sent2 = "";
+
+	vector <string> ::iterator iter = vString.end();
+
+	while (iter != vString.begin())
+	{
+		--iter;
+		sent2 += *iter + " ";
+	}
+
+	sent2 = sent2.substr(0,sent2.length() - 1);
+
+	return sent2;
+}
+
+
+int main()
+{
+	
+	string Sent1 = My_Input_Lib::ReadUsersString("Enter your sentence: ");
+	cout << "Sentence after reversing: " << ReverseWords(Sent1);
+
+	system("pause>0");
+	return 0;
+}
