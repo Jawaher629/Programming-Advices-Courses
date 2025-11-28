56046a310e80db4a70ab9bbfff223ba18c04786c
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2025-11-28 19:11:13 +0300

    Update Problem #44: Remove all punctuations from string.cpp

--- 07 - Algorithms & Problem Solving Level 3/Problem #44: Remove all punctuations from string.cpp
+++ 07 - Algorithms & Problem Solving Level 3/Problem #44: Remove all punctuations from string.cpp
@@ -5,7 +5,8 @@
 using namespace std;
 
 
-string RenameAllPuncuations(string Sent1)
+
+string RemoveAllPuncuations(string Sent1)
 {
 	string Sent2 = "";
 	for (short i = 0; i < Sent1.length(); i++)
@@ -20,8 +21,9 @@ int main()
 	
 	string Sent1 = My_Input_Lib::ReadUsersString("Enter your sentence: ");
 	cout << "Original String:\n" << Sent1;
-	cout << "\n\nPauncuations Removed:\n" << RenameAllPuncuations(Sent1);
+	cout << "\n\nPauncuations Removed:\n" << RemoveAllPuncuations(Sent1);
 
+	
 	system("pause>0");
 	return 0;
 }
