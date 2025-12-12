1cfbb6048ec4e6e7621485fc5caabeed8415d2aa
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2025-12-13 00:38:05 +0300

    Update Problem #44: Remove all punctuations from string.cpp
    The lesson is about building a program removes all punctuation in a giving sentence.
    
    This lesson have taught me a new way to solve problems .
    I tried to solve it with a built in function once and with out it in another way and they both improved my logic.

--- 07 - Algorithms & Problem Solving Level 3/Problem #44: Remove all punctuations from string.cpp
+++ 07 - Algorithms & Problem Solving Level 3/Problem #44: Remove all punctuations from string.cpp
@@ -1,11 +1,50 @@
+//Solution1:
 #include <iostream>
 #include <string>
-#include <vector>
-#include "My_Input_Lib.h"
 using namespace std;
 
+string ReadUserString()
+{
+	string Sentence = "";
+	cout << "Enter Your Sentence: ";
+	cin >> Sentence;
+
+	return Sentence;
+}
+
+bool IsPunc(char Ch)
+{
+	return ((Ch == '.') || (Ch == ',') || (Ch == ';') || (Ch == ':') || (Ch == '!') || (Ch == '?'));
+}
+
+string RemovePuncuations(string Sent1)
+{
+	string Sent2 = "";
+	for (short i = 0; i < Sent1.length(); i++)
+	{
+		if (!IsPunc(Sent1[i]))
+		{
+			Sent2 += Sent1[i];
+
+		}
+	}
+	return Sent2;
+}
+
+int main()
+{
+	string Sent = "Here is the fucking SA, It's such a shitty countre. ma god help us to get out of here!!!";
+	cout << RemovePuncuations(Sent);
+
+	system("pause>0");
+	return 0;
+}
 
 
+
+
+
+// Solution2:
 string RemoveAllPuncuations(string Sent1)
 {
 	string Sent2 = "";
