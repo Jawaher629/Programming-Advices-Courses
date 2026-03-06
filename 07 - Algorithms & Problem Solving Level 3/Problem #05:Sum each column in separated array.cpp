c3b205161f5c14da996583d42d0a59e299f48cde
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2026-03-06 10:03:26 +0300

    Create Problem #05:Sum each column in separated array.cpp

---
+++ 07 - Algorithms & Problem Solving Level 3/Problem #05:Sum each column in separated array.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int RandomeNumbers(int From, int To)
+{
+	int Randome = rand() % (To - From + 1) + From;
+	return Randome;
+}
+
+
+void FillInTheMatrixRandomly(int arr1[3][3], short Rows, short Colomns)
+{
+	for (short r = 0; r < Rows; r++)
+	{
+		for (short c = 0; c < Colomns; c++)
+		{
+			arr1[r][c] = RandomeNumbers(1, 100);
+		}
+	}
+}
+
+void PrintMatrix(int arr1[3][3], short Rows, short Colomns)
+{
+	for (short r = 0; r < Rows; r++)
+	{
+		for (short c = 0; c < Colomns; c++)
+			cout << setw(3) << arr1[r][c] << "     ";
+		cout << "\n";
+	}
+}
+
+int SumEachColumnInMatrix(int arr1[3][3], short Row, short ColumnsNumber)
+{
+	int sum = 0;
+
+	for (short i = 0; i < Row ; i++)
+	{
+		sum += arr1[i][ColumnsNumber];
+	}
+	return sum;
+}
+
+void PrintEachColumnSum(int arr1[3][3], short Rows, short Column)
+{
+	cout << "The sum of each sum in the matrix: " << endl;
+	for (short i = 0; i < Rows; i++)
+	{
+		cout << "Column #" << i + 1 << " Sum = " << SumEachColumnInMatrix(arr1, Rows, i) << endl;
+	}
+}
+
+
+void SumColsInArray(int arr1[3][3], int arrSum[3], short Rows, short Columns)
+{
+	for (short i = 0; i < Columns; i++)
+	{
+		arrSum[i] = SumEachColumnInMatrix(arr1, 3, i);
+	}
+}
+
+void PrintColumnsSumArray(int arrSum[3], short Columns)
+{
+	cout << "Sum array printed: \n";
+	for (short i = 0; i < Columns; i++)
+	{
+		cout << "Column # " << i + 1 << " sum: " << arrSum[i] << endl;
+	}
+}
+
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int arr1[3][3], arrSum[3];
+	FillInTheMatrixRandomly(arr1, 3, 3);
+	cout << "A 3*3 randome matrix filled: \n";
+	PrintMatrix(arr1, 3, 3);
+	
+	SumColsInArray(arr1, arrSum, 3, 3);
+	PrintColumnsSumArray(arrSum, 3);
+
+	return 0;
+}
