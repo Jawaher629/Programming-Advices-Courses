6d3795e3deb6cb895ac26034c0f246b484447011
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2026-04-13 20:13:12 +0300

    Update Problem #16: Check sparce matrix.cpp

--- 07 - Algorithms & Problem Solving Level 3/Problem #16: Check sparce matrix.cpp
+++ 07 - Algorithms & Problem Solving Level 3/Problem #16: Check sparce matrix.cpp
@@ -68,3 +68,75 @@ int main()
 	
 	return 0;
 }
+
+// Dr. Abo Hadhoud solution:
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int RandomeNumbers(int From, int To)
+{
+	int Randome = rand() % (To - From + 1) + From;
+	return Randome;
+}
+
+
+void FillInTheMatrixRandomly(int arr1[3][3], short Rows, short Colomns)
+{
+	for (short r = 0; r < Rows; r++)
+	{
+		for (short c = 0; c < Colomns; c++)
+		{
+			arr1[r][c] = RandomeNumbers(1, 10);
+		}
+	}
+}
+void PrintMatrix(int arr1[3][3], short Rows, short Columns)
+{
+	for (short r = 0; r < Rows; r++)
+	{
+		for (short c = 0; c < Columns; c++)
+			printf( "%0*d    ", 2,arr1[r][c]);
+		cout << "\n";
+	}
+	cout << endl;
+}
+
+int CountNumberInMatrix(int arr1[3][3], short Rows, short Cols, short Num)
+{
+	int Counter = 0;
+	for (short i = 0; i < Rows; i++)
+		for (short j = 0; j < Cols; j++)
+		{
+			if (arr1[i][j] == Num)
+				Counter++;
+		}
+	return Counter;
+}
+
+
+bool IsSparceMatrix(int arr1[3][3], short Rows, short Cols)
+{
+	short MatrixSize = Rows * Cols;
+	return (CountNumberInMatrix(arr1, 3, 3, 0) >= (MatrixSize / 2));
+}
+
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int arr1[3][3] = { {0,0,7},{0,0,8},{0,0,9} };
+
+	FillInTheMatrixRandomly(arr1, 3, 3);
+	cout << "Matrix: \n";
+	PrintMatrix(arr1, 3, 3);
+	if (IsSparceMatrix(arr1, 3, 3))
+		cout << "Yes, It is Sparce matrix.\n";
+	else
+		cout << "No, It is NOT sparce matrix.\n";
+
+	
+	return 0;
+}
