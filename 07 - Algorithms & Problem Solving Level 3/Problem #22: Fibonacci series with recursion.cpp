9c83f0fae5bb071210fa929a5de784d22fb67e3e
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2026-04-16 17:41:54 +0300

    Update Problem #22: Fibonacci series with recursion.cpp

--- 07 - Algorithms & Problem Solving Level 3/Problem #22: Fibonacci series with recursion.cpp
+++ 07 - Algorithms & Problem Solving Level 3/Problem #22: Fibonacci series with recursion.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+
+void PrintFibonacciSeiresWithRecursion(short Num,int Prev1, int Prev2)
+{
+	int FibNumber = 0;
+	if (Num > 0)
+	{
+		FibNumber = Prev1 + Prev2;
+		Prev1 = Prev2;
+		Prev2 = FibNumber;
+		cout << FibNumber << ",  ";
+	}
+	PrintFibonacciSeiresWithRecursion(Num - 1, Prev1, Prev2);
+}
+
+
+int main()
+{
+
+	PrintFibonacciSeiresWithRecursion(10, 1, 0);
+	
+	return 0;
+}
