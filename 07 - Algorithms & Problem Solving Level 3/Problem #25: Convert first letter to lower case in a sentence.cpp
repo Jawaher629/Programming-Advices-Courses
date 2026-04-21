bd7808adcb8b8e34c5eb8e652df9fe3900689816
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2026-04-21 13:11:44 +0300

    Create Problem #25: Convert first letter to lower case in a sentence.cpp

---
+++ 07 - Algorithms & Problem Solving Level 3/Problem #25: Convert first letter to lower case in a sentence.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+string ReadStringFromUser(string Message)
+{
+	string Sent = "";
+	cout << Message;
+	getline(cin, Sent);
+	return Sent;
+}
+
+string ConvertFirstLetterToLower(string Sent)
+{
+	bool isFirstLetter = true;
+	for (short i = 0; i < Sent.length(); i++)
+	{
+		if (Sent[i] != ' ' && isFirstLetter)
+		{
+			Sent[i] = tolower(Sent[i]);
+		}
+		isFirstLetter = (Sent[i] == ' ' ? true : false);
+	}
+	return Sent;
+}
+
+int main()
+{
+	string Sent = ReadStringFromUser("Enter your sentence: \n");
+	cout << "\n\nConverting first letter of each word to lower case: \n" << ConvertFirstLetterToLower(Sent);
+
+	
+	return 0;
+}
