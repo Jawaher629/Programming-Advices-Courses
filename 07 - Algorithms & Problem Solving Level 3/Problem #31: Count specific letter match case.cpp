f56165f8ce83bc02318b92506e1a79cdadbc4c27
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2026-04-24 20:45:32 +0300

    Create Problem #31: Count specific letter match case.cpp

---
+++ 07 - Algorithms & Problem Solving Level 3/Problem #31: Count specific letter match case.cpp
@@ -0,0 +1,91 @@
+//My solution:
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+char ReadCharFromUser(string Message)
+{
+	cout << Message;
+	char Char;
+	cin >> Char;
+	return Char;
+}
+string ReadStringFromUser(string Message)
+{
+	cout << Message;
+	string Sent = "";
+	getline(cin, Sent);
+	return Sent;
+}
+
+short CountSpecificLetter(string Sent1, char Letter)
+{
+	short Counter = 0;
+	for (short i = 0; i < Sent1.length(); i++)
+	{
+		if (tolower(Sent1[i]) == tolower(Letter))
+			Counter++;
+	}
+	return Counter;
+}
+
+int main()
+{
+	string Sent1 = ReadStringFromUser("Enter your sentence: ");
+	char Letter = ReadCharFromUser("Enter a letter tp count: ");
+
+	cout << "\nLetter [ " << Letter << " ] found: " << CountSpecificLetter(Sent1, Letter) << " times.\n";
+	return 0;
+}
+
+//Dr.Abo-Hadhoud solution:
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+char ReadCharFromUser(string Message)
+{
+	cout << Message;
+	char Char;
+	cin >> Char;
+	return Char;
+}
+string ReadStringFromUser(string Message)
+{
+	cout << Message;
+	string Sent = "";
+	getline(cin, Sent);
+	return Sent;
+}
+
+short CountSpecificLetter(string Sent1, char Letter, bool MatchCase = true)
+{
+	short Counter = 0;
+
+	if (MatchCase)
+	{
+		for (short i = 0; i < Sent1.length(); i++)
+		{
+			if (Sent1[i] == Letter)
+				Counter++;
+		}
+	}
+	else
+	for (short i = 0; i < Sent1.length(); i++)
+	{
+		if (tolower(Sent1[i]) == tolower(Letter))
+			Counter++;
+	}
+	return Counter;
+}
+
+int main()
+{
+	string Sent1 = ReadStringFromUser("Enter your sentence: ");
+	char Letter = ReadCharFromUser("Enter a letter tp count: ");
+
+	cout << "\nLetter [ " << Letter << " ] found: " << CountSpecificLetter(Sent1, Letter,false) << " times.\n";
+	return 0;
+}
