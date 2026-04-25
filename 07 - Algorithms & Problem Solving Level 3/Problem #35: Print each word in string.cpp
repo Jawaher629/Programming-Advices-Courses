6ff1dcc17c065126c0248356adc50387f0627213
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2026-04-25 22:06:04 +0300

    Update Problem #35: Print each word in string.cpp

--- 07 - Algorithms & Problem Solving Level 3/Problem #35: Print each word in string.cpp
+++ 07 - Algorithms & Problem Solving Level 3/Problem #35: Print each word in string.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+string ReadStringFromUser(string Message)
+{
+	cout << Message;
+	string Sent = "";
+	getline(cin, Sent);
+	return Sent;
+}
+
+void PrintEachWordInString(string Sent1, string delim)
+{
+	short pos = 0;
+	string sWord;
+	cout << "Your string words: \n";
+	while ((pos = Sent1.find(delim)) != std::string::npos)
+	{
+		sWord = Sent1.substr(0, pos);
+		if (sWord != "")
+			cout << "- " << sWord << endl;
+		Sent1.erase(0, pos + delim.length());
+	}
+	if (Sent1 != "")
+		cout << "- " << Sent1 << endl;
+}
+int main()
+{
+	string Sent1 = ReadStringFromUser("Enter your sentence: ");
+	PrintEachWordInString(Sent1, " ");
+	
+	return 0;
+}
