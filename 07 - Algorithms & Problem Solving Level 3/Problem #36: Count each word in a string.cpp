89a41a1eb9be0cad88ba62c797c260c088d55509
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2026-04-25 22:12:57 +0300

    Create Problem #36: Count each word in a string.cpp

---
+++ 07 - Algorithms & Problem Solving Level 3/Problem #36: Count each word in a string.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+string ReadStringFromUser(string Message)
+{
+	cout << Message;
+	string Sent = "";
+	getline(cin, Sent);
+	return Sent;
+}
+
+void PrintEachWordInString(string Sent1, string delim)
+{
+	short counter = 0;
+	short pos = 0;
+	string sWord;
+	cout << "Your string words: \n";
+	while ((pos = Sent1.find(delim)) != std::string::npos)
+	{
+		sWord = Sent1.substr(0, pos);
+		if (sWord != "")
+			cout << "- " << sWord << endl;
+		Sent1.erase(0, pos + delim.length());
+		counter++;
+	}
+	if (Sent1 != "")
+		cout << "- " << Sent1 << endl;
+	cout << "\nWords count: " << counter << "words.";
+}
+int main()
+{
+	string Sent1 = ReadStringFromUser("Enter your sentence: ");
+	PrintEachWordInString(Sent1, " ");
+	
+	return 0;
+}
