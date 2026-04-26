8a14b60d052952392fe9d47a0d53e9cc26353546
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2026-04-26 21:03:03 +0300

    Update Problem #38: TrimRight \ TrimLeft\ Trim functions.cpp

--- 07 - Algorithms & Problem Solving Level 3/Problem #38: TrimRight \ TrimLeft\ Trim functions.cpp
+++ 07 - Algorithms & Problem Solving Level 3/Problem #38: TrimRight \ TrimLeft\ Trim functions.cpp
@@ -1,1 +1,52 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
+string ReadStringFromUser(string Message)
+{
+	cout << Message;
+	string Sent = "";
+	getline(cin, Sent);
+	return Sent;
+}
+
+static string TrimLeft(string Sent1)
+{
+	for (short i = 0; i < Sent1.length(); i++)
+	{
+		if (Sent1[i] != ' ')
+		{
+			return Sent1.substr(i, Sent1.length() - i);
+		}
+	}
+	return "";
+}
+
+static string TrimRight(string Sent1)
+{
+	for (int i = Sent1.length() - 1; i >= 0; i--)
+	{
+		if (Sent1[i] != ' ')
+		{
+			return Sent1.substr(0, i + 1);
+		}
+	}
+	return "";
+
+}
+
+string TrimFiunc(string Sent1)
+{
+	return TrimRight(TrimLeft(Sent1));
+}
+
+int main()
+{
+	string Sent1 = ReadStringFromUser("Enter your sentence: ");
+	cout << "\nTrim Left: "<< TrimLeft(Sent1);
+	cout << "\nTrim Right: "<< TrimRight(Sent1);
+	cout << "\nTrim Function:" << TrimFiunc(Sent1);
+
+	return 0;
+}
