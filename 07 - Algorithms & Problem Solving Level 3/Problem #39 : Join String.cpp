b634fa1f9f6ae09e17f89ed569fb45738eab4bff
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2026-04-26 21:21:44 +0300

    Update Problem #39 : Join String.cpp

--- 07 - Algorithms & Problem Solving Level 3/Problem #39 : Join String.cpp
+++ 07 - Algorithms & Problem Solving Level 3/Problem #39 : Join String.cpp
@@ -1,10 +1,55 @@
-string JoinString(vector <string> vWords, string Delim)
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+string ReadStringFromUser(string Message)
+{
+	cout << Message;
+	string Sent = "";
+	getline(cin, Sent);
+	return Sent;
+}
+
+vector <string> SplitString(string Sent1, string delim)
+{
+	vector <string> vWords;
+	short pos = 0;
+	string sWord;
+	while ((pos = Sent1.find(delim)) != std::string::npos)
+	{
+		sWord = Sent1.substr(0, pos);
+		if (sWord != "")
+			vWords.push_back(sWord);
+		Sent1.erase(0, pos + delim.length());
+	}
+	if (Sent1 != "")
+		vWords.push_back(Sent1);
+	return vWords;
+}
+
+void PrintWordsFromVec(vector <string> vWords)
+{
+	cout << "\nTolkens: " << vWords.size() << endl;
+	for (string i : vWords)
+		cout << "- " << i << endl;
+}
+
+string JoinFunc(vector<string> vWords)
 {
 	string Sent1 = "";
-	for (string& s : vWords)
-	{
-		Sent1 += s + Delim;
-	}
-	return Sent1.substr(0, Sent1.length() - Delim.length());
-	
+	for (string s : vWords)
+		Sent1 += s + " ";
+
+	return Sent1;
+}
+
+int main()
+{
+	string Sent1 = ReadStringFromUser("Enter your sentence: ");
+	vector <string > vWords = SplitString(Sent1, " ");
+	cout << "Spilit sentence: "; PrintWordsFromVec(vWords);
+	cout << "Join sentence: " << JoinFunc(vWords);
+
+	return 0;
 }
