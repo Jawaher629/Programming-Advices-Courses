6909facf9402736ddefe07d25b48f7abee426861
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2026-06-15 11:03:47 +0300

    Update Problem #15: Last day, Last month.cpp

--- 08 - Algorithms & Problem Solving Level 4/Problem #15: Last day, Last month.cpp
+++ 08 - Algorithms & Problem Solving Level 4/Problem #15: Last day, Last month.cpp
@@ -1,1 +1,74 @@
+//My Solution
+#include <iostream>
+#include <string>
+using namespace std;
+struct stDate {
+	int Year = 0;
+	int Month = 0;
+	int Day = 0;
+};
 
+bool IsLeapYear(int Number)
+{
+	return ((Number % 4 == 0 && Number % 100 != 0) || Number % 400 == 0 ? true : false);
+
+}
+
+ short ReadUserNumber(string Message)
+{
+	 short num = 0;
+	cout << Message;
+	cin >> num;
+	return num;
+}
+ int DaysInMonth(int Year, int Month)
+ {
+	 if (Month < 1 || Month > 12)
+		 return 0;
+
+	 if (Month == 2)
+		 return (IsLeapYear(Year) ? 29 : 28);
+
+
+	 return ((Month == 4 || Month == 6 || Month == 9 || Month == 11) ? 30 : 31);
+
+ }
+
+ bool IsLastDayInMonth(stDate Date)
+ {
+	 int DaysIMonth = DaysInMonth(Date.Year, Date.Month);
+	 return (Date.Day == DaysIMonth);
+}
+
+ bool IsLastMonthInYear(stDate Date)
+ {
+	 return (Date.Month == 12);
+ }
+
+ stDate ReadFullDate()
+ {
+	 stDate Date;
+	 Date.Year = ReadUserNumber("Enter the year: ");
+	 Date.Month = ReadUserNumber("Enter the month: ");
+	 Date.Day = ReadUserNumber("Enter the day: ");
+
+	 return Date;
+ }
+
+int main()
+{
+	stDate Date1 = ReadFullDate();
+
+	if (IsLastDayInMonth(Date1))
+		cout << "Yes, Day last day in month.\n";
+	else
+		cout << "No,Day Not last day of the month\n";
+	
+	if (IsLastMonthInYear(Date1))
+		cout << "Yes, Month last month in year.\n";
+	else
+		cout << "No,Month Not last month of the year\n";
+
+	system("pause>0");
+	return 0;
+} 
