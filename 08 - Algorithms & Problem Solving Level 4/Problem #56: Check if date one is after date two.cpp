0f3190e785abb0620f55f49968545bdf4fe487f1
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2026-07-19 23:54:51 +0300

    Update Problem #56: Check if date one is after date two.cpp

--- 08 - Algorithms & Problem Solving Level 4/Problem #56: Check if date one is after date two.cpp
+++ 08 - Algorithms & Problem Solving Level 4/Problem #56: Check if date one is after date two.cpp
@@ -50,15 +50,7 @@ bool IsLeapYear(int Number)
 	 return (Date.Month == 12);
  }
 
- bool IsItWeekEnd(stDate Date)
- {
-	 return (GetDayIndexs(Date) == 6 || GetDayIndexs(Date) == 5);
- }
-
- bool IsItBusinessDay(stDate Date)
- {
-	 return !IsItWeekEnd(Date);
- }
+ 
 
  string IndexToDayName(int DayIndex)
  {
@@ -71,7 +63,11 @@ bool IsLeapYear(int Number)
 	 return ((Date1.Year < Date2.Year) ? true : (Date1.Year == Date2.Year) ? (Date1.Month < Date2.Month) ? true : (Date1.Month == Date2.Month) ? (Date1.Day < Date2.Day) : false : false);
  }
 
- 
+ bool IsDay1EqualToDay2(stDate Date1, stDate Date2)
+ {
+	 return (Date1.Year == Date2.Year && Date1.Month == Date2.Month && Date1.Day == Date2.Day);
+ }
+
 
  short GetDayIndexs(short Month, short Year, short Day)
  {
@@ -99,7 +95,7 @@ bool IsLeapYear(int Number)
 
  bool IsDate1AfterDate2(stDate Date1, stDate Date2)
  {
-	 return (!IsDay1BeforDay2(Date1, Date2));
+	 return (!IsDay1BeforDay2(Date1, Date2)) && !IsDay1EqualToDay2(Date1,Date2);
  }
 
 int main()
