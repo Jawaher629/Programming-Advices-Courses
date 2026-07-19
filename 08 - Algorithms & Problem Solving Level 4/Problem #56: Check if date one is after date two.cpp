da88bb726c9f3cc796f65a3667329fb5b49319d5
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2026-07-20 00:06:42 +0300

    Update Problem #56: Check if date one is after date two.cpp

--- 08 - Algorithms & Problem Solving Level 4/Problem #56: Check if date one is after date two.cpp
+++ 08 - Algorithms & Problem Solving Level 4/Problem #56: Check if date one is after date two.cpp
@@ -10,14 +10,6 @@ struct stDate
 	int Day = 0;
 };
 
-short GetDayIndexs(short Month, short Year, short Day);
-short GetDayIndexs(stDate Date);
-
-bool IsLeapYear(int Number)
-{
-	return ((Number % 4 == 0 && Number % 100 != 0) || Number % 400 == 0 ? true : false);
-}
-
  short ReadUserNumber(string Message)
 {
 	 short num = 0;
@@ -25,38 +17,6 @@ bool IsLeapYear(int Number)
 	cin >> num;
 	return num;
 }
-
- int DaysInMonth(int Year, int Month)
- {
-	 if (Month < 1 || Month > 12)
-		 return 0;
-
-	 if (Month == 2)
-		 return (IsLeapYear(Year) ? 29 : 28);
-
-
-	 return ((Month == 4 || Month == 6 || Month == 9 || Month == 11) ? 30 : 31);
-
- }
-
- bool IsLastDayInMonth(stDate Date)
- {
-	 int DaysIMonth = DaysInMonth(Date.Year, Date.Month);
-	 return (Date.Day == DaysIMonth);
-}
-
- bool IsLastMonthInYear(stDate Date)
- {
-	 return (Date.Month == 12);
- }
-
- 
-
- string IndexToDayName(int DayIndex)
- {
-	 string DayName[] = { "Sunday","Monday","Tuesday","Wendesday","Thuresday","Friday","Saturday" };
-	 return DayName[DayIndex];
- }
 
  bool IsDay1BeforDay2(stDate Date1, stDate Date2)
  {
@@ -66,20 +26,6 @@ bool IsLeapYear(int Number)
  bool IsDay1EqualToDay2(stDate Date1, stDate Date2)
  {
 	 return (Date1.Year == Date2.Year && Date1.Month == Date2.Month && Date1.Day == Date2.Day);
- }
-
-
- short GetDayIndexs(short Month, short Year, short Day)
- {
-	 short a = (14 - Month) / 12;
-	 short y = Year - a;
-	 short m = Month - (12 * a) - 2;
-	 return (Day + y + (y / 4) - (y / 100) + y / 400 + ((31 * m) / 12)) % 7;
- }
-
- short GetDayIndexs(stDate Date)
- {
-	 return GetDayIndexs(Date.Month, Date.Year, Date.Day);
  }
  
  stDate ReadFullDate(string Message)
