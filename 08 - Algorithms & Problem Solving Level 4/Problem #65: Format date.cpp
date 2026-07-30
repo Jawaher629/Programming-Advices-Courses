3a68f87a00b676b3a5bf4c73ade33898fdfd103a
Jawaher Alshaikh <140131614+Jawaher629@users.noreply.github.com>
2026-07-30 09:57:23 +0300

    Update Problem #65: Format date.cpp

--- 08 - Algorithms & Problem Solving Level 4/Problem #65: Format date.cpp
+++ 08 - Algorithms & Problem Solving Level 4/Problem #65: Format date.cpp
@@ -110,4 +110,103 @@ int main()
 } 
 
 //--------------------------------------------------------------------------------------------------------
-//Dr Abo Hadhoud solution
+//Dr Abo Hadhoud solution: more reusable
+#include <iostream>
+#include <string>
+#include <vector>
+#pragma warning(disable : 4996)
+using namespace std;
+
+struct stDate
+{
+	int Year = 0;
+	int Month = 0;
+	int Day = 0;
+};
+
+ vector <string> SplitString(string Sent1, string delim)
+ {
+	 vector <string> vWords;
+	 short pos = 0;
+	 string sWord;
+	 while ((pos = Sent1.find(delim)) != std::string::npos)
+	 {
+		 sWord = Sent1.substr(0, pos);
+		 if (sWord != "")
+			 vWords.push_back(sWord);
+		 Sent1.erase(0, pos + delim.length());
+	 }
+	 if (Sent1 != "")
+		 vWords.push_back(Sent1);
+	 return vWords;
+ }
+
+ string ReadDateStringFromUer(string Message)
+ {
+	 cout << Message;
+	 string DateString = "";
+	 getline(cin >> ws, DateString);
+
+	 return DateString;
+ }
+
+ stDate StringDateToNumber(string StringDate)
+ {
+	 stDate Date;
+	 vector <string> vecDate = SplitString(StringDate, "/");
+
+	 Date.Day = stoi(vecDate[0]);
+	 Date.Month = stoi(vecDate[1]);
+	 Date.Year = stoi(vecDate[2]);
+
+	 return Date;
+ }
+
+ string NumberDateToString(stDate Date)
+ {
+	 return to_string(Date.Day) + "/" + to_string(Date.Month) + "/" + to_string(Date.Year);
+ }
+
+ string ReplaceWordsInAString(string Sent1, string ToReplace, string TheReplacement)
+ {
+	 short pos = Sent1.find(ToReplace);
+
+	 while (pos != std::string::npos)
+	 {
+		 Sent1 = Sent1.replace(pos, ToReplace.length(), TheReplacement);
+
+		 pos = Sent1.find(ToReplace);
+	 }
+
+	 return Sent1;
+ }
+
+ string DateFormat(stDate Date, string DateFormat = "dd/mm/yyyy")
+ {
+	 string FormattedDate = "";
+
+	 FormattedDate = ReplaceWordsInAString(DateFormat,"dd", to_string(Date.Day));
+	 FormattedDate = ReplaceWordsInAString(FormattedDate,"mm", to_string(Date.Month));
+	 FormattedDate = ReplaceWordsInAString(FormattedDate,"yyyy", to_string(Date.Year));
+
+	 return FormattedDate;
+ }
+
+int main()
+{
+	string StringDate = ReadDateStringFromUer("Enter date in form dd/mm/yyyy: ");
+	stDate Date = StringDateToNumber(StringDate);
+
+	cout << "\nPrint date in diffirent format:\n\n";
+	cout << "- " << DateFormat(Date) << endl;
+	cout << "- " << DateFormat(Date, "yyyy/mm/dd") << endl;
+	cout << "- " << DateFormat(Date, "mm/dd/yyyy") << endl;
+	cout << "- " << DateFormat(Date, "dd-mm-yyyy") << endl;
+	cout << "- " << DateFormat(Date, "mm-dd-yyyy") << endl;
+	cout << "- " << DateFormat(Date, "yyyy-dd-mm") << endl;
+	cout << "- " << DateFormat(Date, "yyyy-mm-dd") << endl;
+	cout << "- " << DateFormat(Date, "\nDay: dd\nMonth: mm\nYear: yyyy") << endl;
+
+	system("pause>0");
+	return 0;
+} 
